538672710d34163cd7acebb5c3b0c4c82edd4f58
Ellis Trickett <ellis.trickett@gmail.com>
2020-12-29 14:18:19 +0000

    adding the register method to the auth service

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -19,6 +19,11 @@ login(model: any) {
           localStorage.setItem('token', user.token);
         }
       })
-    )
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + "register", model);
+
   }
 }
