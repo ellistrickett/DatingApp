7a18442003dce8803ed76eb41a1badb79b294951
Ellis Trickett <ellis.trickett@gmail.com>
2021-01-03 15:39:56 +0000

    ...

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -7,24 +7,25 @@ namespace DatingApp.API.Data
 {
     public class Seed
     {
-        public static void SeedUsers(DataContext context)
+        public static void SeedUsers(DataContext context) 
         {
-            if (!context.Users.Any())
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+            foreach (var user in users)
             {
-              var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
-              var users = JsonConvert.DeserializeObject<List<User>>(userData);
-              foreach (var user in users)
-              {
-                byte[] passwordhash, passwordSalt;
-                CreatePasswordHash("password", out passwordhash, out passwordSalt);
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
 
-                user.PasswordHash = passwordhash;
+                user.PasswordHash = passwordHash;
                 user.PasswordSalt = passwordSalt;
                 user.Username = user.Username.ToLower();
+
                 context.Users.Add(user);
-              }
             }
+
+            context.SaveChanges();
         }
+
         private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using(var hmac = new System.Security.Cryptography.HMACSHA512()) 
--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -12,24 +12,24 @@ namespace DatingApp.API
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
+          var host = CreateWebHostBuilder(args).Build();
+          using (var scope = host.Services.CreateScope())
+          {
+            var services = scope.ServiceProvider;
+            try
             {
-                var services = scope.ServiceProvider;
-                try
-                {
-                  var context = services.GetRequiredService<DataContext>();
-                  context.Database.Migrate();
-                  Seed.SeedUsers(context);
-                }
-                catch (Exception ex)
-                {
-                  var logger = services.GetRequiredService<ILogger<Program>>();
-                  logger.LogError(ex, "An error occured during migration");
-                }
+              var context = services.GetRequiredService<DataContext>();
+              context.Database.Migrate();
+              Seed.SeedUsers(context);
             }
+            catch (Exception ex)
+            {
+              var logger = services.GetRequiredService<ILogger<Program>>();
+              logger.LogError(ex, "An error occured during migration");
+            }
+          }
 
-            host.Run();
+          host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
