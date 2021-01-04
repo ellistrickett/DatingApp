4a07a6c4725e84967b12dca2d36920a691ca8e13
Ellis Trickett <ellis.trickett@gmail.com>
2021-01-04 17:54:20 +0000

    Creating the Member Detailed View component class

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -32,6 +33,7 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
+      MemberDetailComponent
    ],
   imports: [
     BrowserModule,
